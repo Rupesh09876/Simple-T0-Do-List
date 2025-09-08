7ac0ae01c58ebaa55c8deb0384ab93eef56ba2ae
Rupesh09876 <rupeshkatuwal53@gmail.com>
2025-09-08 20:49:47 +0545

    Initial commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,13 @@ import React from "react";
 
 function Navbar() {
   return (
-    <>
-      <nav className="navbar">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            My To-Do List
-          </a>
-        </div>
-      </nav>
-    </>
+    <nav className="navbar">
+      <div className="container-fluid">
+        <span className="navbar-brand">
+          My To-Do List
+        </span>
+      </div>
+    </nav>
   );
 }
 
